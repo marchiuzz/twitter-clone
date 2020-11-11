e5d4e71659e91ffecee9adf29a5c588cb4a01852
marchiuzz <marchiuzz@gmail.com>
2020-11-11 18:05:14 +0200

    installed fortify

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Laravel\Fortify\Fortify;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Fortify::loginView(function () {
+    return view('auth.login');
+});
+Fortify::registerView(function () {
+    return view('auth.register');
 });
