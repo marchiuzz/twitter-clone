493e651e15d8c725bac2d0df24343adf0e0eb242
marchiuzz <marchiuzz@gmail.com>
2020-11-12 11:38:49 +0200

    follow user

--- resources/views/_friends-links.blade.php
+++ resources/views/_friends-links.blade.php
@@ -1,13 +1,13 @@
-<h3 class="font-bold text-xl">Friends</h3>
+<h3 class="font-bold text-xl">Follows</h3>
 
 <ul>
 
-   @foreach(range(1, 8) as $index)
+   @foreach(auth()->user()->follows as $user)
 
     <li class="mb-4">
         <div class="flex items-center text-sm">
-            <img src="https://i.pravatar.cc/40" class="rounded-full mr-2">
-            John Doe
+            <img src="{{$user->avatar}}" class="rounded-full mr-2">
+            {{$user->name}}
         </div>
     </li>
     @endforeach
